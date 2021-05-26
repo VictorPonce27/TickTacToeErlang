1d936384278875417c4f42c1f7b95074261cc26c
VictorP <A00827302@itesm.mx>
2021-05-26 18:58:15 -0500

    first commit

---
+++ player.erl
@@ -0,0 +1,42 @@
+-module(player).
+
+-export([print/1, register_with_server/1]).
+
+% Call like: player:register_with_server('server@DESKTOP-V6V2QAT').
+register_with_server(ServerName) ->
+    % central_server is a name known at the server side.
+    {central_server, ServerName} ! {register, self()},
+    receive
+      {success, symbol, AssignedSymbol} -> AssignedSymbol
+    end,
+    AssignedSymbol.
+
+% signOut(ServerName) ->
+%     {central_server, ServerName} ! {exit, self()},
+%     receive
+%         {done,} ->
+%     end,
+%     _.
+
+print(ServerName) ->
+    {central_server, ServerName} ! {print, self()},
+    receive {success, GameBoard} -> GameBoard,
+    board(GameBoard,0)
+end. 
+
+board(Board, X) ->
+    if X < 3 ->
+	   io:fwrite("~w", [tuple_to_list(element(X, Board))]),
+	   board(Board, X + 1);
+       true ->
+	   io:fwrite("~w", [tuple_to_list(element(X, Board))])
+    end.
+
+% move(ServerName, Move,Symbol) ->
+%     {central_server, ServerName} ! {play, self(), Move},
+
+%     receive
+%         {}
+
+% Check that we can also do erpc:call('server@DESKTOP-V6V2QAT', server, get_players,[]).
+
---
+++ server.erl
@@ -0,0 +1,122 @@
+-module(server).
+
+-export([game_controller/1, get_players/0,
+	 start_server/0]).
+
+% The server must be started first
+% It will initialize the state and will spawn a process that will be listening to messages from the clients.
+% In the messages we will need to handle registration, plays, etc.
+% The clients will know the name of the server and even the registered name so they can send messages directly.
+% -- e.g. In a client, we call {central_server NodeName} ! MSG
+
+handle_player_reg(PlayerId, GameStatus) ->
+    CurrentPlayers = maps:get(players, GameStatus),
+    NumOfPlayers = length(CurrentPlayers),
+    case NumOfPlayers of
+      0 ->
+	  RegistrationResult = {success, symbol, "X"},
+	  NewPlayersList = [PlayerId],
+	  NewGameStatus = maps:put(players, NewPlayersList,
+				   GameStatus),
+	  {RegistrationResult, NewGameStatus};
+      1 ->
+	  RegistrationResult = {success, symbol, "O"},
+	  NewPlayersList = CurrentPlayers ++ PlayerId,
+	  NewGameStatus = maps:put(players, NewPlayersList,
+				   GameStatus),
+	  {RegistrationResult, NewGameStatus};
+      2 ->
+	  RegistrationResult = {error, "No more players allowed"},
+	  {RegistrationResult,
+	   GameStatus} % We dont modify the status if something is not allowed.
+    end.
+
+game_controller(GameStatus) ->
+    receive
+      {register, PlayerId} ->
+	  io:format("Handling player registration ~n"),
+	  RegistrationResult = handle_player_reg(PlayerId,
+						 GameStatus),
+	  % We extract the result of the function
+	  ResponseForUser = element(1, RegistrationResult),
+	  NewGameStatus = element(2, RegistrationResult),
+	  PlayerId ! ResponseForUser,
+	  game_controller(NewGameStatus);
+      {get_players, From} ->
+	  From ! maps:get(players, GameStatus),
+	  game_controller(GameStatus);
+      % {play, From, Move} ->
+      %     PlayResult = play(Move,GameStatus),
+      %     RespondForUser = element(1,PlauResult),
+      %     NewStatus = element(2,PlayResult),
+      %     Done = element(3,PlayResult),
+      %     game_controller(NewStatus);
+      {print, PlayerId} ->
+	  Board = maps:get(board, GameStatus),
+	  PlayerId ! Board,
+	  game_controller(GameStatus);
+      {exit} -> io:fwrite("See you!")
+    end.
+
+start_server() ->
+    % Here, we initialize the map and status
+    io:format("Starting the server~n"),
+    io:format("Handling game ~n"),
+    Gameboard = {{"-", "-", "-"}, 
+                 {"-", "-", "-"},
+		         {"-", "-", "-"}},
+    %!Game board goes here
+    InitialStatus = #{players => [], board => Gameboard,
+		      score => [0, 0, 0]},
+    Controller_Pid = spawn(?MODULE, game_controller,
+			   [InitialStatus]),
+    register(central_server, Controller_Pid).
+
+% functions like this one can be implemented to get status info in the server.
+% They might be even be called from clients using something like:
+% erpc:call('server@DESKTOP-V6V2QAT', server, get_players,[]).
+get_players() ->
+    central_server ! {get_players, self()},
+    receive Players -> Players end,
+    Players.
+
+% % The externally seen and used function
+register_player(PlayerId) ->
+    case whereis(register_pid) of
+      undefined ->
+	  Register_Pid = spawn(?MODULE, internal_register_player,
+			       []),
+	  register(register_pid, Register_Pid)
+    end,
+    register_pid ! PlayerId.
+
+internal_register_player(PlayersList) ->
+    receive
+      PlayerId ->
+	  CurrentPlayers = length(PlayersList),
+	  case CurrentPlayers of
+	    0 ->
+		PlayerId ! {symbol, "X"},
+		NewPlayersList = [PlayerId],
+		internal_register_player(NewPlayersList);
+	    1 ->
+		PlayerId ! {symbol, "O"},
+		NewPlayersList = PlayersList ++ PlayerId,
+		internal_register_player(NewPlayersList);
+	    2 ->
+		PlayerId ! {error, "No more players allowed"},
+		internal_register_player(PlayersList)
+	  end;
+      exit ->
+	  io:fwrite("Good bye from internal register player")
+    end.
+
+% play(Map) ->
+%     receive
+%         {play, Row, Col, Symbol} ->
+
+
+
+% start_game() ->
+%     spawn(play(#{}))
+
